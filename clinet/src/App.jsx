5d0c88b0f5bc517cbd042d25c12508eb3e4d3ea1
UsmanHussain <mhusmanhussain@gmail.com>
2024-08-03 07:03:52 +0530

    Create pages and routes

--- clinet/src/App.jsx
+++ clinet/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Singin from './pages/Singin'
+import SingUp from './pages/SingUp'
+import Dashbord from './pages/Dashbord'
 
 export default function App() {
   return (
-    <div>
-      App
-    </div>
+   <BrowserRouter>
+     <Routes>
+       <Route path="/" element={<Home />} />
+       <Route path="/about" element={< About/>} />
+       <Route path="/sign-in" element={<Singin/>} />
+       <Route path="/sign-up" element={<SingUp />} />
+       <Route path="/dashboard" element={<Dashbord/>} />
+     </Routes>
+    </BrowserRouter>
   )
+
 }
